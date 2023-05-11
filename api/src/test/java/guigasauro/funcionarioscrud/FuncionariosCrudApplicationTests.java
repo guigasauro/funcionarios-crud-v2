f5a42f28527e5418aa5cac6947c0b61171d675fb
guilherme.ns <guilherme.silva.ex@synchro.com.br>
2023-05-11 11:16:46 -0300

    adding empty spring project

---
+++ api/src/test/java/guigasauro/funcionarioscrud/FuncionariosCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package guigasauro.funcionarioscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
